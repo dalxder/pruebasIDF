510e78e86e23c09ff95a8297f3d3ad52b5944076
Dalxder <dalxder@gmail.com>
2017-10-26 13:50:14 -0500

    Add files via upload

--- descargaPdf.py
+++ descargaPdf.py
@@ -1,18 +1,28 @@
 import urllib2
 import pyPdf
+import json
+"""
+from pprint import pprint
 
-
+with open('data.json') as data_file:
+    data = json.load(data_file)
+pprint(data)
+"""
 def main():
     nombrePdf="IDF_16035010_TIBU.pdf"
     listaPdf=[nombrePdf,"IDF_11135010_SAUTATA.pdf"]
+    data=[]
     for i, estacionPdf in enumerate(listaPdf):
         download_file("http://www.ideam.gov.co/documents/10182/24541172/%s"%estacionPdf,estacionPdf)
-        data=textoPdfCoef(estacionPdf)
+        data.append([estacionPdf,textoPdfCoef(estacionPdf)])
         #print "procesado %i"%((i+1)/len(listaPdf)*100)
-        print data
-        
- 
-    
+        #print data
+    a=crearJson(data)
+    print(a)
+    #geom = QgsGeometry.fromPolygon([[QgsPoint(pt[0],pt[1])  for pt in geojson['coordinates'][0] [0]]])
+
+
+
 def download_file(download_url,nombrePdf):
     response = urllib2.urlopen(download_url)
     file = open(nombrePdf, 'wb')
@@ -39,21 +49,56 @@ def textoPdfCoef(nombrePdf):
     for page in pdf.pages:
         data=[]
         a=page.extractText().split("TR=100")[1].split("Datos")[0].split(".")
-        print("a",a)
         r=10
-        for i,d in enumerate([2,3,5,10,25,50]):
-            for j,tr in enumerate(["C1","X0","C2"]):
-                if j==0:
-                    re=float(a[r*i+j][1:]+"."+a[r*i+1+j][:3])
-                    print("R",re)
-                    data.append(re)
+        for i,d in enumerate([2,3,5,10,25,50,100]):
+            #for j,tr in enumerate(["C1","X0","C2"]):
+                if i<6 and i>=1:
+                    C1=float(a[r*i][1+len(str(d)):]+"."+a[r*i+1][:3])
+
+
+                elif i==0:
+                    C1=float(a[r*i][1:]+"."+a[r*i+1][:3])
                 else:
-                    re=float(a[r*i+j][3:]+"."+a[r*i+1+j][:3])
-                    
-                    #data.append(re)
-                    print("R",re,"s")
-                
-        print("ok")
+                    None
+                    C1=float(a[r*(i-1)+3][1+len(str(d)):]+"."+a[r*(i-1)+4][:3])
+
+                if i<6:
+                    X0=float(a[r*i+1][3:]+"."+a[r*i+2][:3])
+                    C2=float(a[r*i+2][3:]+"."+a[r*i+3][:3])
+                else:
+                    None
+                    X0=float(a[r*(i-1)+4][3:]+"."+a[r*(i-1)+5][:3])
+                    C2=float(a[r*(i-1)+5][3:]+"."+a[r*(i-1)+6][:3])
+                data.append([C1,X0,C2])
     return data
+
+
+def crearJson(datas):
+    json=""
+
+    tr=[2,3,5,10,25,50,100]
+    for i in range(len(datas)):
+        str_coef=""
+        data=datas[i][1]
+        nombEstacion=datas[i][0]
+        for j,t in enumerate(tr):
+            str_coef+='"tr%s":[%s,%s,%s]'%(t,data[j][0],data[j][1],data[j][2])
+            if j<len(tr)-1:
+                str_coef+=','
+
+        template="""{
+     "type": "Feature",
+     "geometry": {
+        "type": "Point",
+        "coordinates": [125.6, 10.1]
+      },
+     "properties": {
+       "name": "%s",
+       "id":["C1","X0","C2"],
+       "coeficientes":{%s}
+     }
+}\n"""%(nombEstacion,str_coef)
+        json+=template
+    return json
 if __name__ == "__main__":
     main()
